e768d3537e9a84341a80ad3cedf23244705eb5e4
Dhiaa Alshamy <dhodhof@gmail.com>
2020-07-21 20:28:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+
+const newHeader = document.createElement("H1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
