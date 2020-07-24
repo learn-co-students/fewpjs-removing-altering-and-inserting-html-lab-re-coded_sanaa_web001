9ffe15229d28b38e0e126326454d75dbb7954344
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-07-24 16:09:41 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById("main").remove();
+
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "FAhmed is the champion!";
