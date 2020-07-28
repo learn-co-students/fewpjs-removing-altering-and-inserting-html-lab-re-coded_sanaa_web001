9540901022eb9b5e5b5a39429c32426c2369fb6a
Osamah Gharad <osamah.gharred@gmail.com>
2020-07-28 21:27:58 +0000

    finished

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML= "Osamah is the champion";
+document.body.appendChild(newHeader);
