0c2ad642228c3a3e8c7fa132fe937e91fd8169fa
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-08-01 14:14:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const main = document.getElementById('main');
+main.remove();
+
+  let newHeader =document.createElement('h1');
+ newHeader.id='victory';
+
+  let myName='marwa';
+  newHeader.innerHTML=`${myName} is the champion`;
