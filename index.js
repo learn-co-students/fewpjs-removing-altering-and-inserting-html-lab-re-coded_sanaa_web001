780196fec7f7cee834a89e9cc8a0e1164fa8ea60
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-12 06:37:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let main = document.querySelector('main');
+main.remove();
+let newHeader=document.createElement('h1');
+newHeader.id="victory";
+newHeader.nodeName;
+newHeader.innerHTML="NAME is the champion";
+document.body.appendChild(newHeader);
+
