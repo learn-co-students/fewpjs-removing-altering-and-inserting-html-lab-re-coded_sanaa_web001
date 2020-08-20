dcaf0b1811e413b07b602e5855acd0427d8c96a2
Fatima Amin <fatima.amin4official@gmail.com>
2020-08-20 13:59:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let elem = document.getElementById('main') ;
+elem.parentNode.removeChild(elem);
+let newHeader = document.createElement('h1');
+newHeader.id='victory';
+newHeader.textContent ='YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
